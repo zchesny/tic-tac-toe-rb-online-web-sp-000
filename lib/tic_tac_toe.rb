2626b04b1e21c0b1e6f8e04d037976d3c705c870
Zoey Chesny <z.chesny@gmail.com>
2020-04-08 19:02:24 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -31,7 +31,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  !position_taken?(board, index) && index.between?(0,8)
+  index.between?(0,8) && !position_taken?(board, index)
 end
 
 # Define won?, full?, draw?, over?, and winner below
