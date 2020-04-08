b7f4c21851e3f1ff787a9ab97b89f3070f559e52
Zoey Chesny <z.chesny@gmail.com>
2020-04-08 19:29:28 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -94,8 +94,6 @@ def play(board)
   until over?(board)
     turn(board)
   end
-  puts "HELLO here is the board"
-  display_board(board)
   if won?(board)
     puts "Congratulations #{winner(board)}!"
   elsif draw?(board)
