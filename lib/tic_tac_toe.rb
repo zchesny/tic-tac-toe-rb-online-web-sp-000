9d88dffc50fde04cb555547aaa29ae26e59bfeff
Zoey Chesny <z.chesny@gmail.com>
2020-04-08 18:58:51 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -11,7 +11,11 @@ WIN_COMBINATIONS = [
 ]
 
 def display_board(board)
-
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 # Helper Method
