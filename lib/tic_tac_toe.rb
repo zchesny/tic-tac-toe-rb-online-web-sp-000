7a29479b8edc7f6bf64abf8386203bba0aff9d22
Zoey Chesny <z.chesny@gmail.com>
2020-04-08 19:39:50 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -53,8 +53,7 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index) == true
-    player = current_player(board)
-    move(board, index, player)
+    move(board, index, current_player(board))
     display_board(board)
   else
     turn(board)
